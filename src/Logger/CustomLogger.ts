55483b7042f4212d866d059a1968b76b37a840fd
ahmed-com <ahmed0grwan@gmail.com>
2021-08-09 08:06:29 +0200

    built a custom logger

---
+++ src/Logger/CustomLogger.ts
@@ -0,0 +1,58 @@
+import { InternalServerErrorException, Logger } from '@nestjs/common';
+import {appendFileSync} from 'fs';
+import * as path from 'path'
+// const path = require('path')
+
+export class CustomLogger extends Logger {
+  private logger: Logger;
+
+  constructor(args?: any) {
+    super(args);
+
+    this.logger = new Logger(args);
+  }
+
+  log(message: string, context?: string): void {
+    this.logger.log(message);
+    if(context && context === 'dev') return;
+
+    const generalLogPath = path.join(
+      __dirname,
+      '..',
+      path.sep,
+      '..',
+      path.sep,
+      'logs',
+      path.sep,
+      'general.log'
+    );
+
+    try {
+      appendFileSync(generalLogPath, message + '\n')
+    } catch (err) {
+      throw new InternalServerErrorException('The Logger Failed');
+    }
+  }
+
+  error(message: string, context?: string): void {
+    this.logger.error(message);
+    if(context && context === 'dev') return;
+
+    const errorLogPath = path.join(
+      __dirname,
+      '..',
+      path.sep,
+      '..',
+      path.sep,
+      'logs',
+      path.sep,
+      'error.log'
+    );
+
+    try {
+      appendFileSync(errorLogPath, message + '\n')
+    } catch (err) {
+      throw new InternalServerErrorException('The Logger Failed');
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomLogger } from './Logger/CustomLogger';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
-  const logger = new Logger();
+  const logger = new CustomLogger();
   const app = await NestFactory.create(AppModule);
   // app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = 3000 || process.env.PORT;
   await app.listen(port);
-  logger.log(`Application listening on port ${port}`);
+  logger.log(`Application listening on port ${port}`, process.env.STAGE);
 }
 bootstrap();
